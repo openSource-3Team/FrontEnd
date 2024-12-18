6461879b596c894c7d70d14b3e1bdb1d95934c29
juyeonnnn <opff5423@gmail.com>
2024-12-18 14:32:05 +0900

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vite.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'], // 문제가 되는 모듈을 추가
+      external: [], // 외부 모듈 비우기
     },
   },
-};
+});
