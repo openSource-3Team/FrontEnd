c690f457ea4936af9265e119108735dae1f751cc
juyeonnnn <opff5423@gmail.com>
2024-12-18 13:44:23 +0900

    edit navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -11,7 +11,32 @@ const Navbar = () => {
   useEffect(() => {
     const userid = localStorage.getItem('userid');
     setIsLoggedIn(!!userid);
+
+    if (userid) {
+      fetchNotifications(userid);
+    }
   }, []);
+
+  const fetchNotifications = async (userid) => {
+    try {
+      const response = await fetch(
+        `http://15.165.223.198:3000/users/received/${userid}`,
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setNotificationCount(data.result?.length || 0); // 알림 개수 설정
+      } else {
+        console.error('알림 데이터를 가져오는 데 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('알림 데이터를 가져오는 중 오류:', error);
+    }
+  };
 
   const handleAuthNavigation = (path) => {
     const userid = localStorage.getItem('userid');
@@ -19,7 +44,6 @@ const Navbar = () => {
       alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
       navigate('/login');
     } else {
-      console.log(`Navigating to: ${path}`); // 디버깅용 로그 추가
       navigate(path);
     }
   };
@@ -52,18 +76,16 @@ const Navbar = () => {
               LOGIN
             </LogButton>
           )}
-          <>
-            <ProfileButton onClick={() => handleAuthNavigation('/profile')}>
-              <ProfileIcon />
-              PROFILE
-            </ProfileButton>
-            <NotificationWrapper
-              onClick={() => handleAuthNavigation('/notification')}
-            >
-              <NotificationIcon />
-              {notificationCount > 0 && <Badge>{notificationCount}</Badge>}
-            </NotificationWrapper>
-          </>
+          <ProfileButton onClick={() => handleAuthNavigation('/profile')}>
+            <ProfileIcon />
+            PROFILE
+          </ProfileButton>
+          <NotificationWrapper
+            onClick={() => handleAuthNavigation('/notification')}
+          >
+            <NotificationIcon />
+            {notificationCount > 0 && <Badge>{notificationCount}</Badge>}
+          </NotificationWrapper>
         </RightSection>
       </Top>
       <Bottom>
@@ -104,13 +126,10 @@ const NavbarContainer = styled.div`
 `;
 
 const Top = styled.div`
-  /* Desktop layout */
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 35px 40px;
-
-  /* Responsive layout */
   @media (max-width: 768px) {
     flex-direction: column;
     padding: 30px 20px;
@@ -122,18 +141,13 @@ const Name = styled(Link)`
   color: #333;
   text-decoration: none;
   font-weight: 600;
-  margin-left: 50px;
   display: flex;
   align-items: center;
-
   &:hover {
     color: #333;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
   }
-
-  /* Responsive font size */
   @media (max-width: 768px) {
-    margin-left: 0;
     font-size: 27px;
   }
 `;
@@ -141,7 +155,6 @@ const Name = styled(Link)`
 const LeftSection = styled.div`
   display: flex;
   align-items: center;
-  /* On smaller screens, center the content */
   @media (max-width: 768px) {
     justify-content: center;
     width: 100%;
@@ -151,12 +164,7 @@ const LeftSection = styled.div`
 const MidSection = styled.div`
   color: #333;
   font-size: 20px;
-
-  /* On smaller screens, center and reduce font size */
   @media (max-width: 768px) {
-    margin-right: 0;
-    margin-top: 20px;
-    margin-bottom: 20px;
     font-size: 20px;
     text-align: center;
     width: 100%;
@@ -168,81 +176,17 @@ const Highlight = styled.span`
   font-size: 24px;
   color: #a72b0c;
   margin-right: 15px;
-
   @media (max-width: 768px) {
     font-size: 25px;
-    margin-right: 10px;
   }
 `;
 
 const RightSection = styled.div`
   display: flex;
   gap: 15px;
-  margin: 0 20px;
-  /* On smaller screens, center the elements */
   @media (max-width: 768px) {
     justify-content: center;
     width: 100%;
-    margin-bottom: 20px;
-  }
-`;
-
-const Bottom = styled.div`
-  border-top: 2px solid #333;
-  border-bottom: 2px solid #333;
-  padding: 3px 40px;
-  background-color: white;
-
-  /* On smaller screens, adjust padding */
-  @media (max-width: 768px) {
-    padding: 10px 20px;
-  }
-`;
-
-const BottomSection = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 15px;
-
-  /* On smaller screens, adjust layout */
-  @media (max-width: 768px) {
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 10px;
-  }
-`;
-
-const NavButton = styled.div`
-  /* Button style */
-  background: none;
-  color: #333;
-  border: none;
-  padding: 10px 10px;
-  margin: 0 6px;
-  cursor: pointer;
-  font-size: 20px;
-
-  &:hover {
-    color: #333;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-  }
-`;
-
-const ProfileButton = styled.div`
-  display: flex;
-  align-items: center;
-  background: none;
-  color: #333;
-  border: none;
-  padding: 8px 10px;
-  margin: 0 20px;
-  cursor: pointer;
-  font-size: 20px;
-  text-decoration: none;
-
-  &:hover {
-    color: #333;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
   }
 `;
 
@@ -250,6 +194,7 @@ const NotificationWrapper = styled.div`
   margin: 0 10px;
   position: relative;
   display: flex;
+
   align-items: center;
   justify-content: center;
   cursor: pointer;
@@ -262,14 +207,15 @@ const NotificationIcon = styled(FaBell)`
 
 const Badge = styled.div`
   position: absolute;
-  top: 1px;
-  right: -7px;
-  background-color: #a72b0c; /* 배경색 */
+  top: -1px;
+  right: -10px;
+  background-color: #a72b0c; /* 포인트 색상 */
   color: white; /* 텍스트 색상 */
-  font-size: 10px;
+  font-size: 12px;
   font-weight: bold;
-  border-radius: 100%;
-  padding: 3px 7px;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -283,17 +229,58 @@ const LogButton = styled.div`
   border-radius: 8px;
   cursor: pointer;
   font-size: 20px;
-
   display: flex;
   align-items: center;
-
   &:hover {
     background-color: #a72b0c;
     color: white;
   }
 `;
 
-// 아이콘
+const ProfileButton = styled.div`
+  display: flex;
+  align-items: center;
+  background: none;
+  color: #333;
+  padding: 8px 10px;
+  cursor: pointer;
+  font-size: 20px;
+  &:hover {
+    color: #333;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const Bottom = styled.div`
+  border-top: 2px solid #333;
+  border-bottom: 2px solid #333;
+  padding: 3px 40px;
+  background-color: white;
+  @media (max-width: 768px) {
+    padding: 10px 20px;
+  }
+`;
+
+const BottomSection = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 15px;
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const NavButton = styled.div`
+  background: none;
+  color: #333;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  &:hover {
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`;
+
 const HomeIcon = styled(FaHome)`
   color: #a72b0c;
   margin-right: 10px;
